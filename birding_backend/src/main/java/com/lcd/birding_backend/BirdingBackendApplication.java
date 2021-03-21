28151c890ff581f7ab5655906e84e510bbb1270f
Laura Dobie <moving.toyshop@gmail.com>
2021-03-21 18:39:47 +0000

    Backend set up in Spring

---
+++ birding_backend/src/main/java/com/lcd/birding_backend/BirdingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lcd.birding_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdingBackendApplication.class, args);
+	}
+
+}
---
+++ birding_backend/src/test/java/com/lcd/birding_backend/BirdingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcd.birding_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
