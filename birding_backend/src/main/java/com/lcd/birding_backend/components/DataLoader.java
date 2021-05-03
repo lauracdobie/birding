91f81e40ad97dcdabefec43f04bb26a678ab525a
Laura Dobie <moving.toyshop@gmail.com>
2021-05-03 20:20:39 +0100

    Can save all the birds.

--- birding_backend/src/main/java/com/lcd/birding_backend/components/DataLoader.java
+++ birding_backend/src/main/java/com/lcd/birding_backend/components/DataLoader.java
@@ -1,18 +1,32 @@
 package com.lcd.birding_backend.components;
 
+import com.lcd.birding_backend.api.API;
+import com.lcd.birding_backend.models.Bird;
+import com.lcd.birding_backend.models.BirdPayload;
 import com.lcd.birding_backend.repositories.BirdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
-//public class DataLoader implements ApplicationRunner {
-//    @Autowired
-//    BirdRepository birdRepository;
-//
-//    @Override
-//    public void run(ApplicationArguments args) throws Exception {
-//
-//    }
-//}
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+    @Autowired
+    BirdRepository birdRepository;
+
+    @Autowired
+    API api;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<BirdPayload> birdsFromAPI = api.getAllBirds();
+
+        List<Bird> birdObjects = birdsFromAPI.stream().map(bird -> new Bird(bird.getComName(), bird.getSciName(), bird.getLocName(), bird.getObsDt(), bird.getHowMany(), bird.getLat(), bird.getLng())).collect(Collectors.toList());
+
+        birdRepository.saveAll(birdObjects);
+
+    }
+}
