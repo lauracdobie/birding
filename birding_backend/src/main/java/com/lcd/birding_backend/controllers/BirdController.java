7e4c37b76f1e1c38834e0ea7f67c31638e624ec5
Laura Dobie <moving.toyshop@gmail.com>
2021-05-04 21:59:47 +0100

    Got random bird working using the

--- birding_backend/src/main/java/com/lcd/birding_backend/controllers/BirdController.java
+++ birding_backend/src/main/java/com/lcd/birding_backend/controllers/BirdController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class BirdController {
@@ -23,11 +23,45 @@ public class BirdController {
         return new ResponseEntity<>(dbBirds, HttpStatus.OK);
     }
 
+//    @GetMapping (value = "api/birds/random")
+//    public ResponseEntity<List<Bird>> getRandomBird() {
+//        List<Bird> dbBirds = birdRepository.findAll();
+//        Collections.shuffle(dbBirds);
+//        List<Bird> foundBirds = new ArrayList<>();
+//
+//        Bird randomBird = dbBirds.get(0);
+//        foundBirds.add(randomBird);
+//        return new ResponseEntity<>(foundBirds, HttpStatus.OK);
+//    }
+
     @GetMapping (value = "api/birds/random")
-    public ResponseEntity<Bird> getRandomBird() {
+    public ResponseEntity<List<Bird>> getRandomBird() {
         List<Bird> dbBirds = birdRepository.findAll();
-        Collections.shuffle(dbBirds);
-        Bird randomBird = dbBirds.get(0);
-        return new ResponseEntity<>(randomBird, HttpStatus.OK);
+        int max = dbBirds.size() -1;
+        Random random = new Random();
+        int randomIndex = random.nextInt((max - 0) + 0);
+        List<Bird> foundBirds = new ArrayList<>();
+
+        Bird randomBird = dbBirds.get(randomIndex);
+        foundBirds.add(randomBird);
+        return new ResponseEntity<>(foundBirds, HttpStatus.OK);
     }
+
+//    @GetMapping (value = "api/birds/random/{number}")
+//    public ResponseEntity<List<Bird>> getRandomBirds(@RequestParam String number) {
+//        List<Bird> dbBirds = birdRepository.findAll();
+//        Collections.shuffle(dbBirds);
+//        List<Bird> foundBirds = new ArrayList<>();
+//        int convertedNumber = Integer.parseInt(number);
+//        int i = 0;
+//        while (i < convertedNumber - 1){
+//            Bird randomBird = dbBirds.get(i);
+//            foundBirds.add(randomBird);
+//            i ++;
+//        }
+//
+//        return new ResponseEntity<>(foundBirds, HttpStatus.OK);
+//    }
+
+
 }
